321dcb3e187fecd4bd0402165ddef07a3142ec74
baldurk <baldurk@baldurk.org>
2017-04-11 16:37:14 +0100

    Move buffer code into a new file

---
+++ buffers.cpp
@@ -0,0 +1,33 @@
+#include "precompiled.h"
+
+VKAPI_ATTR VkResult VKAPI_CALL vkCreateBuffer(VkDevice device, const VkBufferCreateInfo *pCreateInfo,
+                                              const VkAllocationCallbacks *pAllocator,
+                                              VkBuffer *pBuffer)
+{
+  VkBuffer ret = new VkBuffer_T;
+  ret->size = pCreateInfo->size;
+  *pBuffer = ret;
+  return VK_SUCCESS;
+}
+
+VKAPI_ATTR void VKAPI_CALL vkDestroyBuffer(VkDevice device, VkBuffer buffer,
+                                           const VkAllocationCallbacks *pAllocator)
+{
+  delete buffer;
+}
+
+VKAPI_ATTR void VKAPI_CALL vkGetBufferMemoryRequirements(VkDevice device, VkBuffer buffer,
+                                                         VkMemoryRequirements *pMemoryRequirements)
+{
+  // TODO
+  pMemoryRequirements->alignment = 1;
+  pMemoryRequirements->memoryTypeBits = 0x3;
+  pMemoryRequirements->size = buffer->size;
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL vkBindBufferMemory(VkDevice device, VkBuffer buffer,
+                                                  VkDeviceMemory memory, VkDeviceSize memoryOffset)
+{
+  // TODO
+  return VK_SUCCESS;
+}
--- icd_stubs.cpp
+++ icd_stubs.cpp
@@ -137,38 +137,6 @@ VKAPI_ATTR VkResult VKAPI_CALL vkEndCommandBuffer(VkCommandBuffer commandBuffer)
   return VK_SUCCESS;
 }
 
-VKAPI_ATTR VkResult VKAPI_CALL vkCreateBuffer(VkDevice device, const VkBufferCreateInfo *pCreateInfo,
-                                              const VkAllocationCallbacks *pAllocator,
-                                              VkBuffer *pBuffer)
-{
-  VkBuffer ret = new VkBuffer_T;
-  ret->size = pCreateInfo->size;
-  *pBuffer = ret;
-  return VK_SUCCESS;
-}
-
-VKAPI_ATTR void VKAPI_CALL vkDestroyBuffer(VkDevice device, VkBuffer buffer,
-                                           const VkAllocationCallbacks *pAllocator)
-{
-  delete buffer;
-}
-
-VKAPI_ATTR void VKAPI_CALL vkGetBufferMemoryRequirements(VkDevice device, VkBuffer buffer,
-                                                         VkMemoryRequirements *pMemoryRequirements)
-{
-  // TODO
-  pMemoryRequirements->alignment = 1;
-  pMemoryRequirements->memoryTypeBits = 0x3;
-  pMemoryRequirements->size = buffer->size;
-}
-
-VKAPI_ATTR VkResult VKAPI_CALL vkBindBufferMemory(VkDevice device, VkBuffer buffer,
-                                                  VkDeviceMemory memory, VkDeviceSize memoryOffset)
-{
-  // TODO
-  return VK_SUCCESS;
-}
-
 VKAPI_ATTR VkResult VKAPI_CALL vkCreateSampler(VkDevice device,
                                                const VkSamplerCreateInfo *pCreateInfo,
                                                const VkAllocationCallbacks *pAllocator,

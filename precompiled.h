0cde88e7729a6b42a8f5f9b8d42668bfae009c12
baldurk <baldurk@baldurk.org>
2017-04-11 16:32:02 +0100

    Further tidying of files - consolidate icd_common.h, remove icd prefix

--- precompiled.h
+++ precompiled.h
@@ -8,8 +8,6 @@
 #include <windows.h>
 #endif
 
-typedef unsigned char byte;
-
 #include <assert.h>
 #include <stdint.h>
 #include <algorithm>
@@ -17,4 +15,64 @@ typedef unsigned char byte;
 #include "3rdparty/microprofile.h"
 #include "3rdparty/vk_icd.h"
 #include "3rdparty/vulkan.h"
-#include "icd_common.h"
+
+typedef unsigned char byte;
+
+struct VkDevice_T
+{
+  ~VkDevice_T()
+  {
+    for(VK_LOADER_DATA *q : queues)
+      delete q;
+  }
+
+  uintptr_t loaderMagic;
+  std::vector<VK_LOADER_DATA *> queues;
+};
+
+struct VkCommandBuffer_T
+{
+  uintptr_t loaderMagic;
+};
+
+struct VkDeviceMemory_T
+{
+  VkDeviceSize size;
+  byte *bytes;
+};
+
+struct VkImage_T
+{
+  VkExtent2D extent;
+  byte *pixels = NULL;
+};
+
+struct VkBuffer_T
+{
+  VkDeviceSize size;
+};
+
+struct VkSwapchainKHR_T
+{
+  VkExtent2D extent;
+
+  struct Backbuffer
+  {
+    VkImage im;
+    VkDeviceMemory mem;
+
+#if defined(_WIN32)
+    HDC dc = NULL;
+    HBITMAP bmp = NULL;
+#endif
+  };
+
+  std::vector<Backbuffer> backbuffers;
+
+#if defined(_WIN32)
+  HWND wnd = NULL;
+  HDC dc = NULL;
+#endif
+
+  uint32_t current = 0;
+};

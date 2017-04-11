8358439a099a227d4f2b3d97e28e1f35672ccb92
baldurk <baldurk@baldurk.org>
2017-04-11 18:54:55 +0100

    Add execution of command buffers (albeit very very hacked to vkcube)

--- cmd_exec.cpp
+++ cmd_exec.cpp
@@ -11,10 +11,25 @@ const T &pull(const byte **offs)
   return *ret;
 }
 
+struct GPUState
+{
+  VkViewport view;
+  VkImage target;
+  VkPipeline pipeline;
+  VkDescriptorSet set;
+};
+
+void ClearTarget(VkImage target, const VkClearColorValue &col);
+
+void DrawTriangles(VkImage target, int numVerts, const float *pos, const float *UV,
+                   const float *MVP, const VkImage tex);
+
 void VkCommandBuffer_T::execute() const
 {
   const byte *cur = commandStream.data();
   const byte *end = cur + commandStream.size();
+
+  GPUState state = {0};
 
   while(cur < end)
   {
@@ -33,34 +48,49 @@ void VkCommandBuffer_T::execute() const
       {
         const cmd::BeginRenderPass &data = pull<cmd::BeginRenderPass>(&cur);
 
-        (void)data;
+        VkRenderPass_T::Subpass &subpass = data.renderPass->subpasses[0];
+        VkRenderPass_T::Attachment &att = subpass.colAttachments[0];
+
+        uint32_t col = att.idx;
+
+        state.target = data.framebuffer->attachments[col]->image;
+
+        if(att.clear)
+        {
+          ClearTarget(state.target, data.clearval.color);
+        }
+
         break;
       }
       case Command::EndRenderPass:
       {
         const cmd::EndRenderPass &data = pull<cmd::EndRenderPass>(&cur);
 
-        (void)data;
+        state.target = VK_NULL_HANDLE;
+
+        break;
       }
       case Command::BindPipeline:
       {
         const cmd::BindPipeline &data = pull<cmd::BindPipeline>(&cur);
 
-        (void)data;
+        state.pipeline = data.pipeline;
+
         break;
       }
       case Command::BindDescriptorSets:
       {
         const cmd::BindDescriptorSets &data = pull<cmd::BindDescriptorSets>(&cur);
 
-        (void)data;
+        state.set = data.set;
+
         break;
       }
       case Command::SetViewport:
       {
         const cmd::SetViewport &data = pull<cmd::SetViewport>(&cur);
 
-        (void)data;
+        state.view = data.view;
         break;
       }
       case Command::SetScissors:
@@ -74,7 +104,21 @@ void VkCommandBuffer_T::execute() const
       {
         const cmd::Draw &data = pull<cmd::Draw>(&cur);
 
-        (void)data;
+        // hack since we don't have shaders compiled to reflect...
+
+        const VkBuffer ubo = state.set->binds[0].data.bufferInfo.buffer;
+        VkDeviceSize offs = state.set->binds[0].data.bufferInfo.offset;
+
+        const VkImage tex = state.set->binds[1].data.imageInfo.imageView->image;
+
+        if(ubo && tex)
+        {
+          const float *mvp = (const float *)(ubo->bytes + offs);
+          const float *pos = mvp + 4 * 4;
+          const float *UV = pos + 12 * 3 * 4;
+
+          DrawTriangles(state.target, data.vertexCount, pos, UV, mvp, tex);
+        }
         break;
       }
     }

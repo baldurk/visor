93490209800694d5c72b73f181adc48d2b2a3531
baldurk <baldurk@baldurk.org>
2017-04-15 13:33:06 +0100

    Add shaders for 'vulkanscene' Sascha Willems demo

--- premade_shaders.cpp
+++ premade_shaders.cpp
@@ -69,6 +69,18 @@ void inverse(const float *in, float *out)
   memset(out, 0, sizeof(float) * 16);
 }
 
+void mul4(const float *a, const float *b, float *out)
+{
+  for(size_t x = 0; x < 4; x++)
+  {
+    for(size_t y = 0; y < 4; y++)
+    {
+      out[x * 4 + y] = b[x * 4 + 0] * a[0 * 4 + y] + b[x * 4 + 1] * a[1 * 4 + y] +
+                       b[x * 4 + 2] * a[2 * 4 + y] + b[x * 4 + 3] * a[3 * 4 + y];
+    }
+  }
+}
+
 static float dot(const float4 &a, const float4 &b)
 {
   return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
@@ -444,6 +456,321 @@ void sascha_texture_fs(const GPUState &state, float pixdepth, const float4 &bary
   out.w = 1.0f;
 }
 
+void sascha_vulkanscene_mesh_vs(const GPUState &state, uint32_t vertexIndex, VertexCacheEntry &out)
+{
+  const float *inPos = (const float *)(state.vbs[0].buffer->bytes + state.vbs[0].offset);
+
+  inPos += 11 * vertexIndex;
+
+  const float *inNormal = inPos + 3;
+  const float *inTexCoord = inNormal + 3;
+  const float *inColor = inTexCoord + 2;
+
+  const VkDescriptorBufferInfo &buf = state.set->binds[0].data.bufferInfo;
+  const VkBuffer ubo = buf.buffer;
+  VkDeviceSize offs = buf.offset;
+
+  const float *projection = (const float *)(ubo->bytes + offs);
+  const float *model = projection + 16;
+  const float *normal = model + 16;
+  const float *view = normal + 16;
+  const float *lightpos = view + 16;
+
+  float *outUV = out.interps[0].v;
+  float *outNormal = out.interps[1].v;
+  float *outColor = out.interps[2].v;
+  float *outEyePos = out.interps[3].v;
+  float *outLightVec = out.interps[4].v;
+
+  // outUV = inTexCoord.st;
+  memcpy(outUV, inTexCoord, 2 * sizeof(float));
+
+  // outNormal = normalize(mat3(ubo.normal) * inNormal);
+  for(int row = 0; row < 3; row++)
+  {
+    outNormal[row] = 0.0f;
+
+    for(int col = 0; col < 3; col++)
+      outNormal[row] += normal[col * 4 + row] * inNormal[col];
+  }
+
+  normalize3(outNormal);
+
+  // outColor = inColor;
+  memcpy(outColor, inColor, 3 * sizeof(float));
+
+  // mat4 modelView = ubo.view * ubo.model;
+  float modelView[16];
+  mul4(view, model, modelView);
+
+  // vec4 pos = modelView * inPos;
+  float4 pos = float4(0.0f, 0.0f, 0.0f, 0.0f);
+  for(int row = 0; row < 4; row++)
+  {
+    for(int col = 0; col < 4; col++)
+      pos.v[row] += modelView[col * 4 + row] * (col < 3 ? inPos[col] : 1.0f);
+  }
+
+  // gl_Position = ubo.projection * pos;
+  out.position = float4(0.0f, 0.0f, 0.0f, 0.0f);
+  for(int row = 0; row < 4; row++)
+  {
+    for(int col = 0; col < 4; col++)
+      out.position.v[row] += projection[col * 4 + row] * pos.v[col];
+  }
+
+  // outEyePos = vec3(modelView * pos);
+  for(int row = 0; row < 3; row++)
+  {
+    outEyePos[row] = 0.0f;
+
+    for(int col = 0; col < 4; col++)
+      outEyePos[row] += modelView[col * 4 + row] * pos.v[col];
+  }
+
+  // vec4 lightPos = vec4(ubo.lightpos, 1.0) * modelView;
+  float4 lightPos = float4(0.0f, 0.0f, 0.0f, 0.0f);
+  for(int row = 0; row < 3; row++)
+  {
+    for(int col = 0; col < 4; col++)
+      lightPos.v[row] += modelView[row * 4 + col] * (col < 3 ? lightpos[col] : 1.0f);
+  }
+
+  // outLightVec = normalize(lightPos.xyz - outEyePos);
+  outLightVec[0] = lightPos.x - outEyePos[0];
+  outLightVec[1] = lightPos.y - outEyePos[1];
+  outLightVec[2] = lightPos.z - outEyePos[2];
+
+  normalize3(outLightVec);
+}
+
+static float specpart(float4 L, float4 N, float4 H)
+{
+  if(dot(N, L) > 0.0f)
+    return powint<64>(clamp(dot(H, N), 0.0f, 1.0f));
+
+  return 0.0f;
+}
+
+void sascha_vulkanscene_mesh_fs(const GPUState &state, float pixdepth, const float4 &bary,
+                                const VertexCacheEntry tri[3], float4 &out)
+{
+  float4 inNormal;
+  inNormal.x = dot(bary, float4(tri[0].interps[1].x, tri[1].interps[1].x, tri[2].interps[1].x, 0.0f));
+  inNormal.y = dot(bary, float4(tri[0].interps[1].y, tri[1].interps[1].y, tri[2].interps[1].y, 0.0f));
+  inNormal.z = dot(bary, float4(tri[0].interps[1].z, tri[1].interps[1].z, tri[2].interps[1].z, 0.0f));
+  inNormal.w = 0.0f;
+
+  float4 inColor;
+  inColor.x = dot(bary, float4(tri[0].interps[2].x, tri[1].interps[2].x, tri[2].interps[2].x, 0.0f));
+  inColor.y = dot(bary, float4(tri[0].interps[2].y, tri[1].interps[2].y, tri[2].interps[2].y, 0.0f));
+  inColor.z = dot(bary, float4(tri[0].interps[2].z, tri[1].interps[2].z, tri[2].interps[2].z, 0.0f));
+  inColor.w = 0.0f;
+
+  float4 inEyePos;
+  inEyePos.x = dot(bary, float4(tri[0].interps[3].x, tri[1].interps[3].x, tri[2].interps[3].x, 0.0f));
+  inEyePos.y = dot(bary, float4(tri[0].interps[3].y, tri[1].interps[3].y, tri[2].interps[3].y, 0.0f));
+  inEyePos.z = dot(bary, float4(tri[0].interps[3].z, tri[1].interps[3].z, tri[2].interps[3].z, 0.0f));
+  inEyePos.w = 0.0f;
+
+  float4 inLightVec;
+  inLightVec.x =
+      dot(bary, float4(tri[0].interps[4].x, tri[1].interps[4].x, tri[2].interps[4].x, 0.0f));
+  inLightVec.y =
+      dot(bary, float4(tri[0].interps[4].y, tri[1].interps[4].y, tri[2].interps[4].y, 0.0f));
+  inLightVec.z =
+      dot(bary, float4(tri[0].interps[4].z, tri[1].interps[4].z, tri[2].interps[4].z, 0.0f));
+  inLightVec.w = 0.0f;
+
+  // vec3 Eye = normalize(-inEyePos);
+  float4 Eye = float4(-inEyePos.x, -inEyePos.y, -inEyePos.z, 0.0f);
+  normalize3(Eye);
+
+  // vec3 Reflected = normalize(reflect(-inLightVec, inNormal));
+  //   with reflect(I, N) = I - 2.0 * dot(N, I) * N;
+  // vec3 Reflected = normalize((-inLightVec) - 2.0 * dot(inNormal, -inLightVec) * inNormal);
+  float4 Reflected;
+  float mulResult =
+      2.0f * dot(inNormal, float4(-inLightVec.x, -inLightVec.y, -inLightVec.z, -inLightVec.w));
+  Reflected.x = -inLightVec.x - mulResult * inNormal.x;
+  Reflected.y = -inLightVec.y - mulResult * inNormal.y;
+  Reflected.z = -inLightVec.z - mulResult * inNormal.z;
+  Reflected.w = 0.0f;
+  normalize3(Reflected);
+
+  // vec3 halfVec = normalize(inLightVec + inEyePos);
+  float4 halfVec =
+      float4(inLightVec.x + inEyePos.x, inLightVec.y + inEyePos.y, inLightVec.z + inEyePos.z, 0.0f);
+  normalize3(halfVec);
+
+  // float diff = clamp(dot(inLightVec, inNormal), 0.0, 1.0);
+  float diff = clamp(dot(inLightVec, inNormal), 0.0f, 1.0f);
+
+  // float spec = specpart(inLightVec, inNormal, halfVec);
+  float spec = specpart(inLightVec, inNormal, halfVec);
+
+  // float intensity = 0.1 + diff + spec;
+  float intensity = 0.1f + diff + spec;
+
+  // vec4 IAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+  float IAmbient = 0.2f;
+
+  // vec4 IDiffuse = vec4(0.5, 0.5, 0.5, 0.5) * max(dot(inNormal, inLightVec), 0.0);
+  float IDiffuse = 0.5f * std::max(dot(inNormal, inLightVec), 0.0f);
+
+  const float shininess = 0.75f;
+
+  // vec4 ISpecular = vec4(0.5, 0.5, 0.5, 1.0) * pow(max(dot(Reflected, Eye), 0.0), 2.0) *
+  // shininess;
+  float ISpecular = 0.5f * powint<2>(std::max(dot(Reflected, Eye), 0.0f)) * shininess;
+
+  // outFragColor = vec4((IAmbient + IDiffuse) * vec4(inColor, 1.0) + ISpecular);
+  out.x = (IAmbient + IDiffuse) * inColor.x + ISpecular;
+  out.y = (IAmbient + IDiffuse) * inColor.y + ISpecular;
+  out.z = (IAmbient + IDiffuse) * inColor.z + ISpecular;
+
+  // Some manual saturation
+  if(intensity > 0.95)
+  {
+    // outFragColor *= 2.25;
+    out.x *= 2.25f;
+    out.y *= 2.25f;
+    out.z *= 2.25f;
+  }
+  else if(intensity < 0.15)
+  {
+    // outFragColor = vec4(0.1);
+    out.x = out.y = out.z = 0.1f;
+  }
+
+  if(out.x > 1.0f)
+    out.x = 1.0f;
+  if(out.y > 1.0f)
+    out.y = 1.0f;
+  if(out.z > 1.0f)
+    out.z = 1.0f;
+
+  out.w = 1.0f;
+}
+
+void sascha_vulkanscene_logo_fs(const GPUState &state, float pixdepth, const float4 &bary,
+                                const VertexCacheEntry tri[3], float4 &out)
+{
+  float4 inNormal;
+  inNormal.x = dot(bary, float4(tri[0].interps[1].x, tri[1].interps[1].x, tri[2].interps[1].x, 0.0f));
+  inNormal.y = dot(bary, float4(tri[0].interps[1].y, tri[1].interps[1].y, tri[2].interps[1].y, 0.0f));
+  inNormal.z = dot(bary, float4(tri[0].interps[1].z, tri[1].interps[1].z, tri[2].interps[1].z, 0.0f));
+  inNormal.w = 0.0f;
+
+  float4 inColor;
+  inColor.x = dot(bary, float4(tri[0].interps[2].x, tri[1].interps[2].x, tri[2].interps[2].x, 0.0f));
+  inColor.y = dot(bary, float4(tri[0].interps[2].y, tri[1].interps[2].y, tri[2].interps[2].y, 0.0f));
+  inColor.z = dot(bary, float4(tri[0].interps[2].z, tri[1].interps[2].z, tri[2].interps[2].z, 0.0f));
+  inColor.w = 0.0f;
+
+  float4 inEyePos;
+  inEyePos.x = dot(bary, float4(tri[0].interps[3].x, tri[1].interps[3].x, tri[2].interps[3].x, 0.0f));
+  inEyePos.y = dot(bary, float4(tri[0].interps[3].y, tri[1].interps[3].y, tri[2].interps[3].y, 0.0f));
+  inEyePos.z = dot(bary, float4(tri[0].interps[3].z, tri[1].interps[3].z, tri[2].interps[3].z, 0.0f));
+  inEyePos.w = 0.0f;
+
+  float4 inLightVec;
+  inLightVec.x =
+      dot(bary, float4(tri[0].interps[4].x, tri[1].interps[4].x, tri[2].interps[4].x, 0.0f));
+  inLightVec.y =
+      dot(bary, float4(tri[0].interps[4].y, tri[1].interps[4].y, tri[2].interps[4].y, 0.0f));
+  inLightVec.z =
+      dot(bary, float4(tri[0].interps[4].z, tri[1].interps[4].z, tri[2].interps[4].z, 0.0f));
+  inLightVec.w = 0.0f;
+
+  // vec3 Eye = normalize(-inEyePos);
+  float4 Eye = float4(-inEyePos.x, -inEyePos.y, -inEyePos.z, 0.0f);
+  normalize3(Eye);
+
+  // vec3 Reflected = normalize(reflect(-inLightVec, inNormal));
+  //   with reflect(I, N) = I - 2.0 * dot(N, I) * N;
+  // vec3 Reflected = normalize((-inLightVec) - 2.0 * dot(inNormal, -inLightVec) * inNormal);
+  float4 Reflected;
+  float mulResult =
+      2.0f * dot(inNormal, float4(-inLightVec.x, -inLightVec.y, -inLightVec.z, -inLightVec.w));
+  Reflected.x = -inLightVec.x - mulResult * inNormal.x;
+  Reflected.y = -inLightVec.y - mulResult * inNormal.y;
+  Reflected.z = -inLightVec.z - mulResult * inNormal.z;
+  Reflected.w = 0.0f;
+  normalize3(Reflected);
+
+  // vec4 diff = vec4(inColor, 1.0) * max(dot(inNormal, inLightVec), 0.0);
+  float4 diff;
+  float dotmul = std::max(dot(inLightVec, inNormal), 0.0f);
+  diff.x = inColor.x * dotmul;
+  diff.y = inColor.y * dotmul;
+  diff.z = inColor.z * dotmul;
+  diff.w = 1.0f;
+
+  // float shininess = 0.0;
+  const float shininess = 0.0f;
+
+  // vec4 spec = vec4(1.0, 1.0, 1.0, 1.0) * pow(max(dot(Reflected, Eye), 0.0), 2.5) * shininess;
+  // shininess == 0.0 so cancels out
+  float4 spec(0.0f, 0.0f, 0.0f, 0.0f);
+
+  // outFragColor = diff + spec;
+  out.x = diff.x;
+  out.y = diff.y;
+  out.z = diff.z;
+
+  // outFragColor.a = 1.0;
+  out.w = 1.0f;
+}
+
+void sascha_vulkanscene_skybox_vs(const GPUState &state, uint32_t vertexIndex, VertexCacheEntry &out)
+{
+  const float *inPos = (const float *)(state.vbs[0].buffer->bytes + state.vbs[0].offset);
+
+  inPos += 11 * vertexIndex;
+
+  const VkDescriptorBufferInfo &buf = state.set->binds[0].data.bufferInfo;
+  const VkBuffer ubo = buf.buffer;
+  VkDeviceSize offs = buf.offset;
+
+  const float *projection = (const float *)(ubo->bytes + offs);
+  const float *model = projection + 16;
+
+  float *outUVW = out.interps[0].v;
+
+  // outUVW = inPos;
+  outUVW[0] = inPos[0];
+  outUVW[1] = inPos[1];
+  outUVW[2] = inPos[2];
+
+  // gl_Position = ubo.projection * ubo.model * vec4(inPos.xyz, 1.0);
+  float4 modelMul = float4(0.0f, 0.0f, 0.0f, 0.0f);
+  for(int col = 0; col < 4; col++)
+  {
+    for(int row = 0; row < 4; row++)
+      modelMul.v[row] += model[col * 4 + row] * (col < 3 ? inPos[col] : 1.0f);
+  }
+
+  out.position.x = out.position.y = out.position.z = out.position.w = 0.0f;
+  for(int col = 0; col < 4; col++)
+  {
+    for(int row = 0; row < 4; row++)
+      out.position.v[row] += projection[col * 4 + row] * modelMul.v[col];
+  }
+}
+
+void sascha_vulkanscene_skybox_fs(const GPUState &state, float pixdepth, const float4 &bary,
+                                  const VertexCacheEntry tri[3], float4 &out)
+{
+  VkImage tex = state.set->binds[1].data.imageInfo.imageView->image;
+
+  float u = dot(bary, float4(tri[0].interps[0].x, tri[1].interps[0].x, tri[2].interps[0].x, 0.0f));
+  float v = dot(bary, float4(tri[0].interps[0].y, tri[1].interps[0].y, tri[2].interps[0].y, 0.0f));
+  float w = dot(bary, float4(tri[0].interps[0].z, tri[1].interps[0].z, tri[2].interps[0].z, 0.0f));
+
+  out = sample_cube_wrapped(u, v, w, tex);
+}
+
 static std::map<uint32_t, Shader> premadeShaderMap;
 
 void InitPremadeShaders()
@@ -458,6 +785,21 @@ void InitPremadeShaders()
         std::make_pair<uint32_t, Shader>(4293881502, (Shader)&sascha_textoverlay_fs));
     premadeShaderMap.insert(std::make_pair<uint32_t, Shader>(3054859395, (Shader)&sascha_texture_vs));
     premadeShaderMap.insert(std::make_pair<uint32_t, Shader>(3971494927, (Shader)&sascha_texture_fs));
+
+    premadeShaderMap.insert(
+        std::make_pair<uint32_t, Shader>(1653401353, (Shader)&sascha_vulkanscene_mesh_vs));
+    premadeShaderMap.insert(
+        std::make_pair<uint32_t, Shader>(1068398372, (Shader)&sascha_vulkanscene_mesh_fs));
+    // logo vertex shader - pretty much identical to mesh shader but with hardcoded light position,
+    // which is identical to the value in the UBO anyway
+    premadeShaderMap.insert(
+        std::make_pair<uint32_t, Shader>(685038778, (Shader)&sascha_vulkanscene_mesh_vs));
+    premadeShaderMap.insert(
+        std::make_pair<uint32_t, Shader>(4043689846, (Shader)&sascha_vulkanscene_logo_fs));
+    premadeShaderMap.insert(
+        std::make_pair<uint32_t, Shader>(3977102539, (Shader)&sascha_vulkanscene_skybox_vs));
+    premadeShaderMap.insert(
+        std::make_pair<uint32_t, Shader>(1118272716, (Shader)&sascha_vulkanscene_skybox_fs));
   }
 }
 
